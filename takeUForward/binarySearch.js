07e0b57f07d97956b5d4789c21d15997f030955d
jasmeendesai <jasmeendesai2597@gmail.com>
2024-07-03 01:14:21 +0530

    binary search

--- takeUForward/binarySearch.js
+++ takeUForward/binarySearch.js
@@ -79,4 +79,59 @@ const lowerBound = (arr, n, x)=>{
     return ans;
 }
 
+
 console.log(lowerBound(arr1, n, x))
+
+
+// https://takeuforward.org/arrays/implement-upper-bound/
+
+const upperbound = (arr, n, x) =>{
+    let low = 0
+    let high = n-1
+    let ans = n
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        // maybe an answer
+        // if(x === arr[mid]) return mid
+        
+        if(x < arr[mid]){
+            ans = mid
+            high = mid -1
+        }
+        else{
+            
+            low = mid+1
+        }
+    }
+    return ans;
+}
+
+console.log(upperbound([3, 5, 8, 9, 15, 19], 6, 9))
+
+
+// https://www.geeksforgeeks.org/problems/ceil-the-floor2802/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=ceil-the-floor
+
+
+const getFloorAndCeil = (arr, n, x) =>{
+    let floor = -1
+    let ciel = Infinity
+
+    for(let i=0; i<n; i++){
+        if(arr[i]<=x){
+            floor = Math.max(floor, arr[i])
+        }else{
+            ciel = Math.min(ciel, arr[i])
+        }
+    }
+
+    if(ciel === Infinity) ciel = -1
+    if(floor === x) ciel = x;
+
+    return [floor, ciel]
+}
+
+console.log(getFloorAndCeil([5, 6, 8, 9, 6, 5, 5, 6], 8, 7))
+console.log(getFloorAndCeil([5, 6, 8, 9, 6, 5, 5, 6], 8, 10))
+
+
